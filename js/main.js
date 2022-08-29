d116633373feb94d3aca9e189f499d0e3150d9e9
Mansur <tursunboyevjahongir04@yandex.ru>
2022-08-29 23:37:14 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+ var myForm=document.querySelector(".myForm")
+ var inputText=document.querySelector(".myInput");
+ var btnEl =document.getElementsByTagName("button");
+ var appenga =document.querySelector(".appen");
+//  var myList =document.querySelector("myList")
+
+
+ 
+ let arr=[];
+//  let newUl= document.createElement('ul');
+//  appenga.appendChild(newUl);
+//  newUl.className="list-group myList";
+myForm.addEventListener('submit' ,function(event){
+    event.preventDefault();
+
+    arr.push(inputText.value);
+    inputText.value="";
+    renderUsers(arr);
+
+    
+}) 
+renderUsers(arr);
+function renderUsers(u){
+    appenga.innerHTML=null;
+    for(let i=0;i<arr.length;i++){
+        let listItem=document.createElement('li');
+        listItem.className="p-2 list-group-item";
+        listItem.textContent=u[i];
+        appenga.appendChild(listItem);
+        
+    }
+}
